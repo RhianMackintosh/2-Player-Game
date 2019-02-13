ce7c9e589fcf10f7940b05e04ce06f48e8d1d62a
sadieh0ugh <43001477+sadieh0ugh@users.noreply.github.com>
2019-02-13 13:04:15 +0000

    initial commit base program

---
+++ gamefile.py
@@ -0,0 +1,8 @@
+import pygame
+
+pygame.init()
+
+display_width = 1200
+display_height = 1000
+gameDisplay = pygame.display.set_mode((display_height,display_width))
+pygame.display.set_caption('Game')
